a859039713cad390a7ec21b34ddac6eb2f2b189c
goncalo <goncalolsilva@ua.pt>
2023-11-08 09:52:48 +0000

    started logic of vhdl

--- cyclicRecCheck_24bitParallel/cyclicRecCheck_24bitParallel.vhd
+++ cyclicRecCheck_24bitParallel/cyclicRecCheck_24bitParallel.vhd
@@ -11,13 +11,23 @@ ENTITY remainder_algorithm IS
 END remainder_algorithm;
 
 ARCHITECTURE structure OF remainder_algorithm IS
-	SIGNAL l1Data: STD_LOGIC_VECTOR(7 DOWNTO 0);
+	SIGNAL l1: STD_LOGIC_VECTOR(17 DOWNTO 0);	-- 18 xors in this level
+	SIGNAL l2: STC_LOGIC_VECTOR(11 DOWNTO 0);	-- 12 xors in this level
+	SIGNAL l3: STC_LOGIC_VECTOR(6 DOWNTO 0);	-- 7 xors in this level
+	SIGNAL l4: STC_LOGIC_VECTOR(1 DOWNTO 0);	-- 2 xors in this level
 	COMPONENT gateXOr2
 		PORT (x0, x1: IN STD_LOGIC;
 		  y: OUT STD_LOGIC);
 	END COMPONENT;
 BEGIN
 
+	-------------- Level 1 --------------
+	xor1: gateXOr2 PORT MAP (input(0),  input(1),  l1(0));
+	xor2: gateXOr2 PORT MAP (input(2),  input(3),  l1(1));
+	xor3: gateXOr2 PORT MAP (input(4),  input(5),  l1(2));
+	--- .
+	--- .
+	--- .
 
 END structure;
 
