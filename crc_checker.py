7a617f10bdaeecce4160250d282c366879d225f3
goncalo <goncalolsilva@ua.pt>
2023-11-09 10:42:02 +0000

    improved encoder and added checker

---
+++ crc_checker.py
@@ -0,0 +1,19 @@
+def calculate_crc(data: str, key: str) -> str:
+    # Append k-1 zeros to the data
+    data += '0' * (len(key) - 1)
+    # Convert the data and key to lists of integers
+    data = [int(bit) for bit in data]
+    key = [int(bit) for bit in key]
+    # Perform modulo-2 binary division
+    for i in range(len(data) - len(key) + 1):
+        if data[i] == 1:
+            for j in range(len(key)):
+                data[i + j] ^= key[j]
+    # Return the remainder as a string
+    return ''.join(str(bit) for bit in data[-(len(key) - 1):])
+
+# Example usage:
+data: str = '100100001'#'101010101010101001110100' #'100100'
+key: str = '1101' #'110100111' #'1101'
+crc: str = calculate_crc(data, key)
+print(f"CRC Remainder: {crc}")
